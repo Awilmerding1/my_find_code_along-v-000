395b690e4b1da037d7d2554f802147ebea11f122
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-09 18:18:21 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,7 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  empty = []
   while i > collection.length 
-  empty << yield(collection[i])
   if yield(collection[i])
     return collection[i]
 end
